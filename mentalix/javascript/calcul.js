90648f7436f8124174f3b4248c66abc66c1312b0
Thekester <theophile.avenel@gmail.com>
2020-04-13 12:19:50 +0200

    Mise en commentiare de la corection de calCulSuivant car il y a un problème lors su premeir affichage du calcul: Il n'y a aucun calcul au début du à l'addEventListener click. Le problème est donc à corriger au plus vite

--- mentalix/javascript/calcul.js
+++ mentalix/javascript/calcul.js
@@ -18,6 +18,26 @@ nombre.addEventListener("click", function () {
 });
 
 
+//var calculSuivant = document.getElementById('invisible');
+//calculSuivant.addEventListener("click", function () {
+//document.getElementById('invisible').style.display = 'none'; //Le bouton devient invisible quand on clique sur le bouton calcul suivant
+//document.getElementById("reponse").innerHTML = "<br />"; // On lui dit de ne plus afficher le vrai ou faux
+//document.getElementById("texte").value = ""; //On lui dit de ne pas garder la réponse précédente que l'utilisateur a tapé
+//document.getElementById('check').style.display = 'block' //On fait disparaître le bouton check
+//var nb1 = Math.floor(Math.random() * 8); //On lui dit que nombre 1 est un nombre à tirer aléatoirement de 1 à 9
+//var signe = "+"; // On lui dit de mettre le signe + entre nb1et nb2
+//var nb2 = Math.floor(Math.random() * 8); //On lui dit que nombre 1 est un nombre à tirer aléatoirement de 1 à 9
+//document.getElementById("calcul").textContent=nb1 + signe + nb2; // On lui dit de nous afficher nb1 tiré aléatoirement + le signe+ et enfin nb2 tiré aléatoirement
+//document.getElementById("nb1").textContent = nb1; // On lui dit d'afficher le nouveau nb1 dans la zone nb1
+// document.getElementById("op").textContent = signe; // On lui dit d'afficher le signe d'opération + dans la zone op
+// document.getElementById("nb2").textContent = nb2; // On lui dit d'afficher le nouveau nb2 dans la zone nb2
+
+/*			do function random()&function nombre(event) //refait les fonctions de calculs aléatoires et de vérification de la réponse
+			
+			while onclick calculSuivant() // tant que le bouton calcul suivant est cliqué
+*/
+//});
+
 
 function calculSuivant() { //La fonction calcul suivant
     document.getElementById('invisible').style.display = 'none'; //Le bouton devient invisible quand on clique sur le bouton calcul suivant
@@ -27,7 +47,7 @@ function calculSuivant() { //La fonction calcul suivant
     var nb1 = Math.floor(Math.random() * 8); //On lui dit que nombre 1 est un nombre à tirer aléatoirement de 1 à 9
     var signe = "+"; // On lui dit de mettre le signe + entre nb1et nb2
     var nb2 = Math.floor(Math.random() * 8); //On lui dit que nombre 1 est un nombre à tirer aléatoirement de 1 à 9
-    //document.getElementById("calcul").textContent=nb1 + signe + nb2; // On lui dit de nous afficher nb1 tiré aléatoirement + le signe+ et enfin nb2 tiré aléatoirement
+    document.getElementById("calcul").textContent = nb1 + signe + nb2; // On lui dit de nous afficher nb1 tiré aléatoirement + le signe+ et enfin nb2 tiré aléatoirement
     document.getElementById("nb1").textContent = nb1; // On lui dit d'afficher le nouveau nb1 dans la zone nb1
     document.getElementById("op").textContent = signe; // On lui dit d'afficher le signe d'opération + dans la zone op
     document.getElementById("nb2").textContent = nb2; // On lui dit d'afficher le nouveau nb2 dans la zone nb2
@@ -55,6 +75,7 @@ function calculScore() { //La fonction de calcul du score
 
 
 
+//calculSuivant.addEventListener(function (){});
 
 
 calculSuivant(); //On appelle la fonction calcul suivant
